2e4a277d854f0745306ff21269db9d404ee5a63e
zhuchenxi <973829691@qq.com>
2015-11-07 10:43:51 +0800

    修改toolbar的错误

--- presentation/src/main/java/com/jude/keychain/presentation/widget/PaddingStatusbarFrameLayout.java
+++ presentation/src/main/java/com/jude/keychain/presentation/widget/PaddingStatusbarFrameLayout.java
@@ -1,7 +1,32 @@
 package com.jude.keychain.presentation.widget;
 
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import com.jude.utils.JUtils;
+
 /**
  * Created by zhuchenxi on 15/11/7.
  */
-public class PaddingStatusbarFrameLayout {
+public class PaddingStatusbarFrameLayout extends FrameLayout{
+    public PaddingStatusbarFrameLayout(Context context) {
+        super(context);
+    }
+
+    public PaddingStatusbarFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PaddingStatusbarFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ){
+            int height = JUtils.getStatusBarHeight();
+            setPadding(0,height,0,0);
+        }
+    }
 }
